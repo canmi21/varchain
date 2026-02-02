42b7392d3b820de5361ee54ff8e0eab8d491eb90
Canmi <9997200@qq.com>
2026-02-02 21:15:02 +0800

    feat: add new example for HashMap-based sources

---
+++ examples/hashmap.rs
@@ -0,0 +1,16 @@
+/* examples/hashmap.rs */
+
+use std::collections::HashMap;
+use varchain::Scope;
+
+#[tokio::main]
+async fn main() {
+	let mut map = HashMap::new();
+	map.insert("port".to_owned(), "8080".to_owned());
+
+	let scope = Scope::new().push(map);
+
+	let val = scope.lookup("port").await;
+	println!("port: {val:?}");
+	assert_eq!(val, Some("8080".to_owned()));
+}
