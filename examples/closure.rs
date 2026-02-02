b2c83cdd8cb0bdf327bdf4110aaa241859ae6d9b
Canmi <9997200@qq.com>
2026-02-02 21:15:02 +0800

    feat: add new example for closure-based sources

---
+++ examples/closure.rs
@@ -0,0 +1,20 @@
+/* examples/closure.rs */
+
+use varchain::{Resolved, Scope};
+
+#[tokio::main]
+async fn main() {
+	let closure = |key: &str| {
+		if key == "version" {
+			Resolved::found("1.0.0")
+		} else {
+			Resolved::pass()
+		}
+	};
+
+	let scope = Scope::new().push(closure);
+
+	let val = scope.lookup("version").await;
+	println!("version: {val:?}");
+	assert_eq!(val, Some("1.0.0".to_owned()));
+}
